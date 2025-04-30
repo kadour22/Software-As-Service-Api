efbf63d1c9061415eecf1be81505c8384121a26d
kadour <kadourzaroui399@gmail.com>
2025-04-30 14:37:24 -0400

    Hide the secret key

--- saas_application/settings.py
+++ saas_application/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bb8qt*i+312&#2r=^9^bfu_0=^re+=@2#gfj-@2$ty_9tx7t5m'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
