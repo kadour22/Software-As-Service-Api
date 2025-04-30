76383ffb6d51c874b8e1b0086428d9b3efab4425
kadour <kadourzaroui399@gmail.com>
2025-04-30 14:23:12 -0400

    Add .env file

--- saas_application/settings.py
+++ saas_application/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,11 +81,11 @@ WSGI_APPLICATION = 'saas_application.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':"software_as_a_service",                 
-        'USER':"postgres",                 
-        'PASSWORD': "zaza", 
-        'HOST': "localhost",                
-        'PORT': "5432",                 
+        'NAME':config("NAME"),                 
+        'USER':config("USER"),                 
+        'PASSWORD':config("PASSWORD"), 
+        'HOST': config("HOST"),                
+        'PORT': config("PORT"),                 
     }
 }
 
